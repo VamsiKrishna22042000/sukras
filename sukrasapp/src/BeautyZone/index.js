dcc38e1ef9c000c4d4f3576cf6e75211daaab214
VamsiKrishna22042000 <vamsi45678901@gmail.com>
2023-07-08 22:30:07 +0530

    beautyzone complete

--- sukrasapp/src/BeautyZone/index.js
+++ sukrasapp/src/BeautyZone/index.js
@@ -96,47 +96,48 @@ const Beautyzone = (props) =>{
 
 
     return(
-        load === pageStage.loading ? <div className="loader-spinner"> <TailSpin color={"#F4BD18"} height={70} width={70}/></div>:
-        <div className="sukras-main-beauty">
-        <div className='sukras-header-beauty'>
-                <img className='sukraslogobeauty' src="./sukraslogo.png" alt="Logo Space"/>
-                <button onClick={gobackTo} className="arrow-btn" type="button"><img className="left-arrow-mobile" src="./backarrow.png"/></button>
-                <button className="location-btnn" type="button"><img className="location-mobilee" src="./location-icon.png"/></button>
-                <select className="dropdown-container">
-                    <option>Hyderabad</option>
-                </select>
-                <button onClick={deleteCookie} className="search-btnn" type="button"><img className="search-mobile" src="./search-mobile.png"/></button>
-                <button className="notification-btnn" type="button"><img className="notification-mobile" src="./notification-mobile.png"/></button>
-                <div className="search-cart">
-                    <input className="serch-cart-input" placeholder="Enter keywords, title, author or ISBN " type="search"/>
-                    <button onClick={deleteCookie} className="search-icon-button">
-                        <img src="./search-icon.png" alt="search-icon" className="search-icon"/>
-                    </button>
-                    <Link to={`/cart/beautyzone`}>
-                        <button className="count-of-cart">{itemsInCart.length}</button>
-                        <button className="cart-icon-buttonn">
-                            <img src="./cart.png" alt="cart-icon" className="cart-icon"/>
+        load === pageStage.success ? 
+       
+            <div className="sukras-main-beauty">
+            <div className='sukras-header-beauty'>
+                    <img className='sukraslogobeauty' src="./sukraslogo.png" alt="Logo Space"/>
+                    <button onClick={gobackTo} className="arrow-btn" type="button"><img className="left-arrow-mobile" src="./backarrow.png"/></button>
+                    <button className="location-btnn" type="button"><img className="location-mobilee" src="./location-icon.png"/></button>
+                    <select className="dropdown-container">
+                        <option>Hyderabad</option>
+                    </select>
+                    <button onClick={deleteCookie} className="search-btnn" type="button"><img className="search-mobile" src="./search-mobile.png"/></button>
+                    <button className="notification-btnn" type="button"><img className="notification-mobile" src="./notification-mobile.png"/></button>
+                    <div className="search-cart">
+                        <input className="serch-cart-input" placeholder="Enter keywords, title, author or ISBN " type="search"/>
+                        <button onClick={deleteCookie} className="search-icon-button">
+                            <img src="./search-icon.png" alt="search-icon" className="search-icon"/>
                         </button>
-                    </Link>
-                </div>
-        </div>
-        <div className="beautyzone-body">
-            <Carousel/>
-        </div>
-        <div className="beautyzone-body-2">
-            {categories === "" ? <div className="service-spinner"> <TailSpin color={"#F4BD18"} height={70} width={70}/></div>:
-            <div className="our-services">
-                    <p className="our-services-head">Our Service's</p>
-                    {categories.map(each=>(
-                    <Link  to={`/${each.category}/${each._id}`}>
-                        <button key={each._id} className="our-services-btn" id={each._id} type="button">
-                             <img className="our-services-img" src={`${each.categoryImage}`} alt={each.categoryImage}/>
-                             <p style={{textTransform:"capitalize"}} className="our-services-name">{each.category}</p>
-                        </button>
-                    </Link>))}
-            </div>}
-        </div>
-        </div>
+                        <Link to={`/cart/beautyzone`}>
+                            <button className="count-of-cart">{itemsInCart.length}</button>
+                            <button className="cart-icon-buttonn">
+                                <img src="./cart.png" alt="cart-icon" className="cart-icon"/>
+                            </button>
+                        </Link>
+                    </div>
+            </div>
+            <div className="beautyzone-body">
+                <Carousel/>
+            </div>
+            <div className="beautyzone-body-2">
+                {categories === "" ? <div className="service-spinner"> <TailSpin color={"#F4BD18"} height={70} width={70}/></div>:
+                <div className="our-services">
+                        <p className="our-services-head">Our Service's</p>
+                        {categories.map(each=>(
+                        <Link  to={`/${each.category}/${each._id}`}>
+                            <button key={each._id} className="our-services-btn" id={each._id} type="button">
+                                <img className="our-services-img" src={`${each.categoryImage}`} alt={each.categoryImage}/>
+                                <p style={{textTransform:"capitalize"}} className="our-services-name">{each.category}</p>
+                            </button>
+                        </Link>))}
+                </div>}
+            </div>
+            </div> : <div className="loader-spinner"> <TailSpin color={"#F4BD18"} height={70} width={70}/></div>
         
     )
 }
